71d576b78dfc26763c384321ec8868ee4b1fb535
tacahiroy <tacahiroy@gmail.com>
2012-02-03 13:46:51 +0000

    became somewhat wise

--- _loga_zsh_completion.zsh
+++ _loga_zsh_completion.zsh
@@ -8,13 +8,15 @@ _is_project_dir() {
 _loga_terms() {
   # trim TARGET TERM and NOTE
   terms=(`loga show | sed -e 's/[ ]\{11,\}[^ ].*$//'`)
+  compadd $@ -k terms
 }
 
 _loga_glossaries() {
   glossaries=(`loga list`)
+  compadd $@ -k glossaries
 }
 
-_loga_help_arguments() {
+_loga_tasks() {
   _args=("add"
   "config"
   "delete"
@@ -31,34 +33,45 @@ _loga_help_arguments() {
   compadd "$@" -k _args
 }
 
+_loga_delete_flags() {
+  _args=("--force")
+  compadd "$@" -k _args
+}
+
+_loga_config_flags() {
+  _args=("--global")
+  compadd "$@" -k _args
+}
+
 local -a _1st_arguments
 _1st_arguments=(
-  'add:Add term to glossary'
-  'config:Set config'
-  'delete:Delete term'
-  'help:Describe available tasks or one specific task'
-  'import:Import external glossary'
-  'list:Show glossary list'
-  'lookup:Lookup terms'
-  'new:Create .logaling'
-  'register:Register .logaling'
-  'show:Show terms in glossary'
-  'unregister:Unregister .logaling'
-  'update:Update term'
-  'version:Show version'
+  "add:Add term to glossary"
+  "config:Set config"
+  "delete:Delete term"
+  "help:Describe available tasks or one specific task"
+  "import:Import external glossary"
+  "list:Show glossary list"
+  "lookup:Lookup terms"
+  "new:Create .logaling"
+  "register:Register .logaling"
+  "show:Show terms in glossary"
+  "unregister:Unregister .logaling"
+  "update:Update term"
+  "version:Show version"
 )
 
-local expl
-local -a terms glossaries is_project_dir
+# local -a terms glossaries is_project_dir
+# _is_project_dir
 
-_is_project_dir
+loga_global_flags=(
+  "(-g --glossary=GLOSSARY)"{-g,--glossary=-}"[Set glossary]:glossaries:->glossary"
+  "(-S --source-language=SOURCE-LANGUAGE)"{-S,--source-language=-}"[Set source language]"
+  "(-T --target-language=TARGET-LANGUAGE)"{-T,--target-language=-}"[Set target language]"
+  "(-h --logaling-home=LOGALING-HOME)"{-h,--logaling-home=-}"[Set logaling home]"
+)
 
 _arguments \
-  "(-g --glossary=GLOSSARY)"{-g,--glossary=-}"[Set glossary]:glossaries:->glossary" \
-  "(-S --source-language=SOURCE-LANGUAGE)"{-S,--source-language=-}"[Set source language]" \
-  "(-T --target-language=TARGET-LANGUAGE)"{-T,--target-language=-}"[Set target language]" \
-  "(-h --logaling-home=LOGALING-HOME)"{-h,--logaling-home=-}"[Set logaling home]" \
-  '*:: :->subcmds' && return 0
+  "*:: :->subcmds" && return 0
 
 if (( CURRENT == 1 )); then
   _describe -t commands "loga subcommand" _1st_arguments
@@ -66,46 +79,43 @@ if (( CURRENT == 1 )); then
 fi
 
 case "$words[1]" in
+  add|import|list|new|unregister|version)
+    _arguments \
+      $loga_global_flags
+    ;;
   config)
     _arguments \
-      '(--global)--global[]'
+      ":key:" \
+      ":value:" \
+      ":flags:_loga_config_flags" \
+      $loga_global_flags
     ;;
   delete)
-    if [[ $is_project_dir -eq 1 ]]; then
-      _arguments \
-        ":argument:->term"
-    else
-      _arguments \
-        "(--force)--force[]"
-    fi
+    _arguments \
+      ":term:_loga_terms" \
+      ":flags:_loga_delete_flags" \
+      $loga_global_flags
     ;;
   help)
     _arguments \
-      ":argument:_loga_help_arguments"
+      ":task:_loga_tasks" \
+      $loga_global_flags
     ;;
-  lookup|update)
-    if [[ $is_project_dir -eq 1 ]]; then
-      _arguments \
-        ":argument:->term"
-    fi
+  lookup)
+    _arguments \
+      ":term:_loga_terms" \
+      $loga_global_flags
+    ;;
+  update)
+    _arguments \
+      ":source:_loga_terms" \
+      ":target:" \
+      ":new_target:" \
+      $loga_global_flags
     ;;
   show)
-    if [[ $is_project_dir -eq 0 ]]; then
-      _arguments \
-        '(-g)-g[Please input glossary name]:::->glossary'
-    fi
+    _arguments \
+      $loga_global_flags
     ;;
 esac
 
-case "$state" in
-  glossary)
-    _loga_glossaries
-    _wanted glossaries expl glossary \
-      compadd - $glossaries
-    ;;
-  term)
-    _loga_terms
-    _wanted terms expl term \
-      compadd - "$terms[@]"
-    ;;
-esac

25c9be0b2a33e253c6882c6a56245cf9d5892f38
tacahiroy <tacahiroy@gmail.com>
2012-02-07 23:08:44 +0000

    added License text

--- _loga_zsh_completion.zsh
+++ _loga_zsh_completion.zsh
@@ -1,4 +1,26 @@
 #compdef loga
+# Version: 0.3.0
+# Author: Takahiro YOSHIHARA <tacahiroy```AT```gmail.com>
+# License: MIT License
+# Copyright 2012 Takahiro YOSHIHARA # {{{
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# The software is provided "as is", without warranty of any kind, express or
+# implied, including but not limited to the warranties of merchantability,
+# fitness for a particular purpose and noninfringement. In no event shall the
+# authors or copyright holders be liable for any claim, damages or other
+# liability, whether in an action of contract, tort or otherwise, arising from,
+# out of or in connection with the software or the use or other dealings in the
+# software.
+# }}}
 
 _loga_config_keys() {
   keys=(glossary source-language target-language)
@@ -144,3 +166,5 @@ case "$state" in
     _loga_glossaries
     ;;
 esac
+
+# vim: fdm=marker
